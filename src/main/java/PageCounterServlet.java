a66392cbf75319760c06848fd6a6bdce5d3588d8
Luis Oliverez <louoliverez901@gmail.com>
2023-06-12 13:32:34 -0700

    added page-counter in separate servlet

---
+++ src/main/java/PageCounterServlet.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "PageCounterServlet", urlPatterns = "/count")
+public class PageCounterServlet extends HttpServlet {
+    private int pageViews;
+
+    public void init() throws ServletException {
+        super.init();
+        pageViews = 0;
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String reset = request.getParameter("reset");
+
+        if (reset != null && reset.equals("true")) {
+            pageViews = 0;
+        } else {
+            pageViews++;
+        }
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><h1> Page Views: " + pageViews + "</h1></html>");
+    }
+}
